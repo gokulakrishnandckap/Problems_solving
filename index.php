0c02270eda0bc45e794002c0b1bcad8928e0e64e
dckap <gokulakrishnanddckap@gmail.com>
2023-05-20 14:00:47 +0530

    No of days problem completed

--- index.php
+++ index.php
@@ -3,38 +3,58 @@
 
 
 
-function primeLogic($Num)
-{
-    if($Num == 1)
-    {
-        return false;
-    }
-    for ($i=2; $i < $Num; $i++) { 
+// function primeLogic($Num)
+// {
+//     if($Num == 1)
+//     {
+//         return false;
+//     }
+//     for ($i=2; $i < $Num; $i++) { 
       
-        if($Num % $i == 0)
+//         if($Num % $i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// $Number = 35;
+
+// $result = primeLogic($Number);
+
+//     if(!$result)
+//     {
+//         echo "Not a prime Number";
+//     }
+//     else
+//     {
+//         echo "It's prime Number";
+
+//     }
+
+
+
+    //Problem 2, Write a program to find no of days between two dates in Php?
+
+
+        $first = (string)readline('Enter Start Date(mm/dd/yyyy): ');
+        $second = (string)readline('Enter End Date(mm/dd/yyyy): ');
+
+
+        if($first == $second)
         {
-            return false;
+            echo "Your trying to calculate same dates";
         }
-    }
-    return true;
-}
-$Number = 7;
-
-$result = primeLogic($Number);
-
-    if(!$result)
-    {
-        echo "Not a prime Number";
-    }
-    else
-    {
-        echo "It's prime Number";
-
-    }
+        else
+        {
+            $fristDate = strtotime($first);
+            $currentDate = strtotime($second);
+            $result = ($currentDate - $fristDate)/86400;
+            echo abs($result)."Days";
+        }
 
 
-
-
+     
 
 
 
