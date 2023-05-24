2e01e2ea99308b4aef47cffefd891420359924c8
dckap <gokulakrishnanddckap@gmail.com>
2023-05-24 14:16:01 +0530

    Reverse problem  completed

--- index.php
+++ index.php
@@ -37,24 +37,32 @@
     //Problem 2, Write a program to find no of days between two dates in Php?
 
 
-        $first = (string)readline('Enter Start Date(mm/dd/yyyy): ');
-        $second = (string)readline('Enter End Date(mm/dd/yyyy): ');
+        // $first = (string)readline('Enter Start Date(mm/dd/yyyy): ');
+        // $second = (string)readline('Enter End Date(mm/dd/yyyy): ');
 
 
-        if($first == $second)
-        {
-            echo "Your trying to calculate same dates";
-        }
-        else
-        {
-            $fristDate = strtotime($first);
-            $currentDate = strtotime($second);
-            $result = ($currentDate - $fristDate)/86400;
-            echo abs($result)."Days";
-        }
+        // if($first == $second)
+        // {
+        //     echo "Your trying to calculate same dates";
+        // }
+        // else
+        // {
+        //     $fristDate = strtotime($first);
+        //     $currentDate = strtotime($second);
+        //     $result = ($currentDate - $fristDate)/86400;
+        //     echo abs($result)."Days";
+        // }
 
 
-     
+//Problem 3  
+
+Function reverseNumber($input){
+    $str = str_split($input);
+    for ($i=count($str)-1; $i >= 0 ; $i--) { 
+        echo $str[$i];
+    }
+}
+ echo reverseNumber(123456);
 
 
 
